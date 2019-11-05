dd7ec0d311b74cefb1fe310d57b5c8a65d6d4ff9
steiler <github@vahlenkamp.net>
2019-11-05 12:29:43 +0100

    Fixing indention of error.py

--- ixnetwork_restpy/errors.py
+++ ixnetwork_restpy/errors.py
@@ -24,8 +24,8 @@ from requests import Response
 class IxNetworkError(Exception):
     """The base error class for all IxNetwork REST API errors"""
     def __init__(self, message, status_code=None):
-		self._message = message
-		self._status_code = status_code
+        self._message = message
+        self._status_code = status_code
 
     @property
     def message(self):
